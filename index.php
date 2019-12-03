9e389ac9bf257c32f7dbad0f9374a79cac2466b1
Maxim Gabidullin <gabidullin@gmail.com>
2019-12-03 21:33:10 +0300

    before ubuntu reinstall

---
+++ index.php
@@ -0,0 +1,97 @@
+<?php
+$filename1 = 'sites1.txt';
+$filename2 = 'sites2.txt';
+
+/**
+ * @param $filename
+ * @return array|false|string
+ */function readSites($filename)
+{
+    $sites = file_get_contents($filename);
+    $sites = explode("\n", $sites);
+    array_shift($sites);
+
+    $sites = array_map('trim', $sites);
+    $sites = array_filter($sites, function (string $site) {
+        if (!strlen($site)) {
+            return false;
+        }
+        if ('#' == substr($site, 0, 1)) {
+            return false;
+        }
+        return true;
+    });
+    sort($sites);
+    return $sites;
+}
+
+$sites1 = readSites($filename1);
+$sites2 = readSites($filename2);
+
+?><!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title>kuba.moscow &mdash; создание простых сложных сайтов</title>
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/kuba.moscow.css">
+    <?php require_once 'metrika.html' ?>
+</head>
+<body>
+
+<main class="container">
+    <h2 class="display-4" style="background: yellow">kuba.moscow.ru</h2>
+    <h3>
+        Создание простых сложных сайтов любой направленности,
+        автоматизирование бизнес-процессов вашей конторы
+        в&nbsp;виде веб-сервисов,
+        совсем лаконичный дизайн, моментальный отклик,
+        а&nbsp;ещё удаление вирусов.
+        <br>
+    </h3>
+    <br>
+    <div class="text-center h0">
+        <small>Сегодня дежурный менеджер &mdash;</small>
+        <br>
+        <a class="" href="mailto:max@kuba.moscow">max@kuba.moscow</a>
+    </div>
+
+    <hr size="1">
+
+    <noindex>
+        <div class="row" id="sites">
+            <div class="col-lg-5">
+                <h5 style="background: #">мы додумали, сделали, <br>вывели в свет:</h5>
+                <div class="row">
+                    <?php foreach ($sites1 as $site): ?>
+                        <div class="col-lg-5 col-sm-6">
+                            <a href="http://<?= $site ?>" rel="nofollow" target="_blank" class="site-link"><?= $site ?></a><br>
+                        </div>
+                    <?php endforeach ?>
+                </div>
+                <br>
+                <em>и наверняка много чего-то ещё &mdash; всё не припомнишь.</em>
+            </div>
+            <div class="col-lg-7">
+                <h5>отремонтировали, <br>покрасили, довели до ума:</h5>
+                <div class="row">
+                    <?php foreach ($sites2 as $site): ?>
+                        <div class="col-lg-4 col-sm-6">
+                            <a href="http://<?= $site ?>" rel="nofollow" target="_blank" class="site-link"><?= $site ?></a><br>
+                        </div>
+                    <?php endforeach ?>
+                </div>
+            </div>
+        </div>
+    </noindex>
+    <hr size="1">
+    <div style="background: #333; margin: 1em; padding: 2em;">
+        <h1 style="color: #fff;">Сколько денег?</h1>
+        <div style="color: #fff;">Каждый раз по-разному. Иногда сайт-визитка стоит дороже мега-портала. Надо смотреть, в общем.</div>
+    </div>
+    <br>
+</main>
+<script>
+    // TODO footer вниз $(window).on('')
+</script>
+</body>
+</html>
